94c8751ba46782d7fd8e02afd3621356f130302a
markcapece <mark.capece@gmail.com>
2019-09-04 10:34:11 -0400

    Add comments and change loop parameters

--- src/hangman/Main.java
+++ src/hangman/Main.java
@@ -5,23 +5,25 @@ import java.io.FileNotFoundException;
 public class Main {
 	
 	public static void main(String[] args) throws FileNotFoundException {
-		
+		// Welcome and initialize game
 		System.out.println("Welcome to Hangman");
+		System.out.println("******************");
 		Game game = new Game();
 		
-		while(game.won==0 && game.lost==0) {
+		// Game loop
+		while(game.game_state == 1) {
 			System.out.println(game.return_hidden() + "\n");
 			Gallows.current_gallow(game.number_of_misses);
 			System.out.println("\n" + game.guessed_letters);
 			game.get_guess();
 		}
 		
+		// Terminate game as win or loss
 		if (game.won == 1) {
 			System.out.println("You won! The word is " + game.return_correct());
 		} else {
 			System.out.println("You lost! The word is " + game.return_correct());
 		}
-		
 		game.record_score();
 		
 	}
