4bb254aba87356df8d60144db563796b9facd409
markcapece <mark.capece@gmail.com>
2019-09-04 10:33:41 -0400

    Fix gallows print

--- src/hangman/Gallows.java
+++ src/hangman/Gallows.java
@@ -1,71 +1,72 @@
 package hangman;
 
 abstract class Gallows {
-		
+	
+	// Print number of misses as hanged man
 	public static void current_gallow(int misses) {	
 		switch(misses) {
 		case 0:
-			System.out.println("      -------- ");
-			System.out.println("      |      | ");
-			System.out.println("      |        ");
-			System.out.println("      |        ");
-			System.out.println("      |        ");
-			System.out.println("      |        ");
-			System.out.println("   ------      ");
+			System.out.println("   -------- ");
+			System.out.println("   |      | ");
+			System.out.println("   |        ");
+			System.out.println("   |        ");
+			System.out.println("   |        ");
+			System.out.println("   |        ");
+			System.out.println("------      ");
 			break;
 		case 1:
-			System.out.println("      -------- ");
-			System.out.println("      |      | ");
-			System.out.println("      |      O ");
-			System.out.println("      |        ");
-			System.out.println("      |        ");
-			System.out.println("      |        ");
-			System.out.println("   ------      ");
+			System.out.println("   -------- ");
+			System.out.println("   |      | ");
+			System.out.println("   |      O ");
+			System.out.println("   |        ");
+			System.out.println("   |        ");
+			System.out.println("   |        ");
+			System.out.println("------      ");
 			break;
 		case 2:
-			System.out.println("      -------- ");
-			System.out.println("      |      | ");
-			System.out.println("      |      O ");
-			System.out.println("      |      | ");
-			System.out.println("      |        ");
-			System.out.println("      |        ");
-			System.out.println("   ------      ");
+			System.out.println("   -------- ");
+			System.out.println("   |      | ");
+			System.out.println("   |      O ");
+			System.out.println("   |      | ");
+			System.out.println("   |        ");
+			System.out.println("   |        ");
+			System.out.println("------      ");
 			break;
 		case 3:
-			System.out.println("      -------- ");
-			System.out.println("      |      | ");
-			System.out.println("      |      O/");
-			System.out.println("      |      | ");
-			System.out.println("      |        ");
-			System.out.println("      |        ");
-			System.out.println("   ------      ");
+			System.out.println("   -------- ");
+			System.out.println("   |      | ");
+			System.out.println("   |      O/");
+			System.out.println("   |      | ");
+			System.out.println("   |        ");
+			System.out.println("   |        ");
+			System.out.println("------      ");
 			break;
 		case 4:
-			System.out.println("      -------- ");
-			System.out.println("      |      | ");
-			System.out.println("      |     \\O/");
-			System.out.println("      |      | ");
-			System.out.println("      |        ");
-			System.out.println("      |        ");
-			System.out.println("   ------      ");
+			System.out.println("   -------- ");
+			System.out.println("   |      | ");
+			System.out.println("   |     \\O/");
+			System.out.println("   |      | ");
+			System.out.println("   |        ");
+			System.out.println("   |        ");
+			System.out.println("------      ");
 			break;
 		case 5:
-			System.out.println("      -------- ");
-			System.out.println("      |      | ");
-			System.out.println("      |     \\O/");
-			System.out.println("      |      | ");
-			System.out.println("      |     /  ");
-			System.out.println("      |        ");
-			System.out.println("   ------      ");
+			System.out.println("   -------- ");
+			System.out.println("   |      | ");
+			System.out.println("   |     \\O/");
+			System.out.println("   |      | ");
+			System.out.println("   |     /  ");
+			System.out.println("   |        ");
+			System.out.println("------      ");
 			break;
 		case 6:
-			System.out.println("      -------- ");
-			System.out.println("      |      | ");
-			System.out.println("      |     \\O/");
-			System.out.println("      |      | ");
-			System.out.println("      |     / \\");
-			System.out.println("      |        ");
-			System.out.println("   ------      ");
+			System.out.println("   -------- ");
+			System.out.println("   |      | ");
+			System.out.println("   |     \\O/");
+			System.out.println("   |      | ");
+			System.out.println("   |     / \\");
+			System.out.println("   |        ");
+			System.out.println("------      ");
 			break;
 		default:
 			System.out.println("Error!");
